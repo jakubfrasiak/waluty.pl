a9c889be24e37ebb9bad8057784f3d30d6cea6da
Patrick3969 <87494019+Patrick3969@users.noreply.github.com>
2023-06-05 09:02:07 +0200

    dodanie portfelu

--- src/server.js
+++ src/server.js
@@ -1,23 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import { generateData } from './scripts/generateData.js';
-import {readFile} from 'fs/promises'
+import { readFile } from 'fs/promises'
 export const config = JSON.parse(await readFile('./src/config/config.json', 'utf-8'));
 
 
 const app = express();
 
 app.set('view-engine', 'ejs');
+app.use(express.static('src/public/'));
 app.use(cors());
 
-app.get('/', async (req, res) => {
+app.get('/', async(req, res) => {
     res.status(200);
     res.render('../src/views/index.ejs', {
         data: await generateData(),
         json: JSON.stringify(await generateData()),
         config: JSON.stringify(config)
     });
-    
+
 });
 
 app.listen(config.port, () => console.log(`Serwer został włączony! [http://127.0.0.1:${config.port}/]`));
