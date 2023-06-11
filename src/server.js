c424f1261c9bcbf2f3722d0a5decc3ff9660b396
Graficzekk <71562927+Graficzekk@users.noreply.github.com>
2023-06-11 17:26:18 +0200

    Update server.js
    Dodano routy i middlewary

--- src/server.js
+++ src/server.js
@@ -1,24 +1,57 @@
 import express from 'express';
 import cors from 'cors';
 import { generateData } from './scripts/generateData.js';
-import { readFile } from 'fs/promises'
-export const config = JSON.parse(await readFile('./src/config/config.json', 'utf-8'));
+import {readFile} from 'fs/promises'
 
+import cookieParser from 'cookie-parser';
+
+export const config = JSON.parse(await readFile('./src/config/config.json', 'utf-8'));
 
 const app = express();
 
+app.use(cookieParser())
+
 app.set('view-engine', 'ejs');
-app.use(express.static('src/public/'));
 app.use(cors());
 
-app.get('/', async(req, res) => {
+app.use(express.static('src/public/'));
+app.set('views', 'src/views/');
+
+app.get('/', async (req, res) => {
     res.status(200);
-    res.render('../src/views/index.ejs', {
+    res.render('index.ejs', {
         data: await generateData(),
         json: JSON.stringify(await generateData()),
         config: JSON.stringify(config)
     });
+    
+});
 
+app.get('/portfel', async (req, res) => {
+    if(req.cookies['login'] && req.cookies['password']) {
+        res.status(200);
+        res.render('portfel.ejs', {
+            config: JSON.stringify(config)
+        });
+    } else {
+        res.redirect('/login')
+
+    }
+});
+
+app.get('/login', async (req, res) => {
+    res.status(200);
+    res.render('login.ejs', {
+        config: JSON.stringify(config)
+    });
+});
+
+app.get('*', async (req, res) => {
+	res.status(404);
+	res.render('error.ejs', {
+		config: JSON.stringify(config),
+		error: res.statusCode,
+	});
 });
 
 app.listen(config.port, () => console.log(`Serwer został włączony! [http://127.0.0.1:${config.port}/]`));
