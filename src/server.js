24f1745456c5b96643ac9247be7692f56d152ea4
Patrick3969 <87494019+Patrick3969@users.noreply.github.com>
2023-06-12 09:23:49 +0200

    js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { generateData } from './scripts/generateData.js';
-import {readFile} from 'fs/promises'
+import { readFile } from 'fs/promises'
 
 import cookieParser from 'cookie-parser';
 
@@ -17,20 +17,27 @@ app.use(cors());
 app.use(express.static('src/public/'));
 app.set('views', 'src/views/');
 
-app.get('/', async (req, res) => {
+app.get('/', async(req, res) => {
     res.status(200);
     res.render('index.ejs', {
         data: await generateData(),
         json: JSON.stringify(await generateData()),
         config: JSON.stringify(config)
     });
-    
+
 });
 
-app.get('/portfel', async (req, res) => {
-    if(req.cookies['login'] && req.cookies['password']) {
+app.get('/portfel', async(req, res) => {
+    const requiredCookies = {
+        balance: req.cookies["balance"]
+    }
+    config.currencies.forEach((currency) => requiredCookies[currency] = req.cookies[currency])
+    console.log(requiredCookies)
+    if (req.cookies['login'] && req.cookies['password']) {
         res.status(200);
         res.render('portfel.ejs', {
+            cookies: JSON.stringify(requiredCookies),
+            data: await generateData(),
             config: JSON.stringify(config)
         });
     } else {
@@ -39,19 +46,19 @@ app.get('/portfel', async (req, res) => {
     }
 });
 
-app.get('/login', async (req, res) => {
+app.get('/login', async(req, res) => {
     res.status(200);
     res.render('login.ejs', {
         config: JSON.stringify(config)
     });
 });
 
-app.get('*', async (req, res) => {
-	res.status(404);
-	res.render('error.ejs', {
-		config: JSON.stringify(config),
-		error: res.statusCode,
-	});
+app.get('*', async(req, res) => {
+    res.status(404);
+    res.render('error.ejs', {
+        config: JSON.stringify(config),
+        error: res.statusCode,
+    });
 });
 
 app.listen(config.port, () => console.log(`Serwer został włączony! [http://127.0.0.1:${config.port}/]`));
